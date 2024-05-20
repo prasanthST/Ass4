064c9c759ae070c231f19349db8c6c90a2266cb7
prasanthST <casinoprasanth@gmail.com>
2024-05-20 16:51:25 +0530

    changes made

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-"# script" 
+
+function reqListener (params) {
+    const countryArr = JSON.parse(this.responseText);
+    console.log(countryArr[0]);
+ 
+    for(let country of countryArr){
+     console.table(country.name.common)
+     console.log(country.flags.png)
+     console.log(country.region)
+     console.log(country.population)
+     console.log(country.subregion)
+    }  
+  
+ }
+   const req = new XMLHttpRequest();
+   req.addEventListener("load", reqListener);
+   req.open("GET","https://restcountries.com/v3.1/all");
+   req.send();
