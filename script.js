26c35db525eb46949edf3f271a36fac8e5456c5d
prasanthST <casinoprasanth@gmail.com>
2024-05-20 23:46:20 +0530

    changes made

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
+// question no 1 
+// compare two JSON have same properties
 
+ let obj1 ={name:"person 1" , age:"5"}
+ let obj2 ={age:"5", name:"person 1"}
+
+ let stringifiedObj1 = JSON.stringify(obj1);
+ let stringifiedObj2 = JSON.stringify(obj2);
+ 
+ let isEqual = stringifiedObj1 === stringifiedObj2;
+ 
+ console.log(isEqual); 
+
+
+
+
+
+// question2 
+// print Flag names ?
 function reqListener (params) {
     const countryArr = JSON.parse(this.responseText);
     console.log(countryArr[0]);
  
     for(let country of countryArr){
-        
+
      console.log(country.flags.png)
 
     }  
